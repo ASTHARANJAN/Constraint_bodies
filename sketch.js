ee97265e21a6e41f68486d4c6492245c8c4a279e
ASTHARANJAN <kamleshboi@gmail.com>
2020-12-23 18:26:06 +0530

    ABC

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
+
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg, platform, log6, bird, con;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,9 +35,14 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230,180,80, PI/2);
 
+   
     bird = new Bird(100,100);
+    con = new Chain(bird.body,log6.body);
 
+    
+    
 }
 
 function draw(){
@@ -58,7 +65,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
+    con.display();
     bird.display();
     platform.display();
+
+   
 }
